14dd60999804c68f446bdc9e75e269fc486dd6e8
KirillK54 <safdid3@gmail.com>
2025-04-16 01:59:12 +0300

    Commit ver2.0

--- app/src/main/java/ru/kirillsafonov/tictactoe2/gameactivity/game.java
+++ app/src/main/java/ru/kirillsafonov/tictactoe2/gameactivity/game.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import java.util.Random;
 
 import ru.kirillsafonov.tictactoe2.R;
 import ru.kirillsafonov.tictactoe2.helper.themehelper;
@@ -18,6 +21,7 @@ public class game extends AppCompatActivity {
     Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
 
     String cross, zero;
+    TextView playerwinner;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +39,7 @@ public class game extends AppCompatActivity {
         button7 = findViewById(R.id.button7);
         button8 = findViewById(R.id.button8);
         button9 = findViewById(R.id.button9);
+        playerwinner = findViewById(R.id.player_winner);
     }
 
     public void btn_back(View view) {
@@ -67,29 +72,91 @@ public class game extends AppCompatActivity {
 
     public void btn1(View view) {
         button1.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn2(View view) {
         button2.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn3(View view) {
         button3.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn4(View view) {
         button4.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn5(View view) {
         button5.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn6(View view) {
         button6.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn7(View view) {
         button7.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn8(View view) {
         button8.setText(cross);
+        isPlayerWinner();
+        AI();
     }
     public void btn9(View view) {
         button9.setText(cross);
+        isPlayerWinner();
+        AI();
     }
+
+    public void isPlayerWinner() {
+        if (button1.getText() == cross && button2.getText() == cross && button3.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button4.getText() == cross && button5.getText() == cross && button6.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button7.getText() == cross && button8.getText() == cross && button9.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button1.getText() == cross && button4.getText() == cross && button7.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button2.getText() == cross && button5.getText() == cross && button8.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button3.getText() == cross && button6.getText() == cross && button9.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button1.getText() == cross && button5.getText() == cross && button9.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+        if (button3.getText() == cross && button5.getText() == cross && button7.getText() == cross) {
+            playerwinner.setText(R.string.player_winner);
+        }
+    }
+    public void AI() {
+        Random random = new Random();
+        int AIClick = 1 + random.nextInt(9);
+
+        switch(AIClick) {
+            case (1): button1.setText(zero); break;
+            case (2): button2.setText(zero); break;
+            case (3): button3.setText(zero); break;
+            case (4): button4.setText(zero); break;
+            case (5): button5.setText(zero); break;
+            case (7): button6.setText(zero); break;
+            case (8): button8.setText(zero); break;
+            case (9): button9.setText(zero); break;
+        }
+    }
+
+
 }
